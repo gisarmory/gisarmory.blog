044757e1ed848b19d607c4aa588fbb8d19577ec7
xiaolei <changxin747@126.com>
2020-06-22 21:32:07 +0800

    增加源码的跳转设置

---
+++ config_source.js
@@ -0,0 +1,5 @@
+var sourceList = [{
+    id: "leaflet100ThousandData",
+    label: "leaflet加载10万数据",
+    url: "https://github.com/gisarmory/gisarmory.blog/blob/master/leaflet-100ThousandData/demo.html"
+}];

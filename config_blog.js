299fbc81d0f067a7d7492ead6eb65fcb27c34351
xuyf <xuyf_90@163.com>
2020-07-07 21:40:54 +0800

    格式化代码

--- config_blog.js
+++ config_blog.js
@@ -1,9 +1,9 @@
 var blogList = [{
-    id: "leaflet100ThousandData",
-    label: "leaflet加载10万数据",
-    url: "leaflet-100ThousandData/README.md"
+  id: "leaflet100ThousandData",
+  label: "leaflet加载10万数据",
+  url: "leaflet-100ThousandData/README.md"
 }, {
-    id: "home",
-    label: "首页",
-    url: "README.md"
+  id: "home",
+  label: "首页",
+  url: "README.md"
 }];

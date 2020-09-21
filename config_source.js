3570f480688c80cadea75d7d62e862b9b0814119
liuxiaolei <lxl@m.bjenv.com>
2020-09-21 12:36:45 +0800

    增加源码配置

--- config_source.js
+++ config_source.js
@@ -2,4 +2,8 @@ var sourceList = [{
   id: "leaflet100ThousandData",
   label: "leaflet加载10万数据",
   url: "https://github.com/gisarmory/gisarmory.blog/blob/master/leaflet-100ThousandData/demo.html"
+},{
+  id: "leafletMapCorrection",
+  label: "leaflet互联网地图纠偏插件",
+  url: "https://github.com/gisarmory/Leaflet.InternetMapCorrection/blob/master/src/leaflet.mapCorrection.js"
 }];

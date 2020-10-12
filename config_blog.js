22b07fed07acded379b84f8e82479224a8718ff0
xuyf <xuyf_90@163.com>
2020-10-12 17:47:40 +0800

    Leaflet.ShowHideLayerGroup Leaflet.Path.DashFlow Leaflet.PolylineDecorator

--- config_blog.js
+++ config_blog.js
@@ -1,29 +1,41 @@
-var blogList = [ {
-  id: "blogList",
-  label: "博客列表",
-  url: "README.md"
+var blogList = [{
+    id: "blogList",
+    label: "博客列表",
+    url: "README.md"
 }, {
-  id: "wechat",
-  label: "公众号",
-  url: "menu/wechat.md"
+    id: "wechat",
+    label: "公众号",
+    url: "menu/wechat.md"
 }, {
-  id: "github",
-  label: "github",
-  href:"https://github.com/gisarmory",
+    id: "github",
+    label: "github",
+    href: "https://github.com/gisarmory",
 }, {
-  id: "aboutUs",
-  label: "关于我们",
-  url: "menu/aboutUs.md"
-},{
-  id: "leaflet100ThousandData",
-  label: "leaflet加载10万数据",
-  url: "leaflet-100ThousandData/README.md"
-},{
-  id: "leafletMapCorrection",
-  label: "leaflet互联网地图纠偏插件",
-  url: "leaflet-MapCorrection/README.md"
-},{
-  id: "maputnikGeoserverVectorTiles",
-  label: "maputnik加载geoserver发布的tms矢量瓦片",
-  url: "maputnik-GeoserverVectorTiles/README.md"
+    id: "aboutUs",
+    label: "关于我们",
+    url: "menu/aboutUs.md"
+}, {
+    id: "leaflet100ThousandData",
+    label: "leaflet加载10万数据",
+    url: "leaflet-100ThousandData/README.md"
+}, {
+    id: "leafletMapCorrection",
+    label: "leaflet互联网地图纠偏插件",
+    url: "leaflet-MapCorrection/README.md"
+}, {
+    id: "LeafletShowHideLayerGroup",
+    label: "leaflet LayerGroup图层组控制显示隐藏",
+    url: "leaflet-ShowHideLayerGroup/README.md"
+}, {
+    id: "LeafletPathDashFlow",
+    label: "Leaflet 动态流向线",
+    url: "leaflet-PathDashFlow/README.md"
+}, {
+    id: "LeafletPolylineDecorator",
+    label: "Leaflet 带箭头的线",
+    url: "leaflet-PolylineDecorator/README.md"
+}, {
+    id: "maputnikGeoserverVectorTiles",
+    label: "maputnik加载geoserver发布的tms矢量瓦片",
+    url: "maputnik-GeoserverVectorTiles/README.md"
 }];

aa2542c1c921ba329c08d24c8c52ef9e74e64ceb
xuyf <xuyf_90@163.com>
2021-03-02 13:46:48 +0800

    LeafletRouteAnimate

--- config_blog.js
+++ config_blog.js
@@ -84,7 +84,8 @@ var blogList = [{
 }, {
     id: "LeafletRouteAnimate",
     label: "Leaflet带箭头轨迹以及沿轨迹带方向的动态marker",
-    href: "leaflet-RouteAnimate/README.md"
+    url: "leaflet-RouteAnimate/README.md",
+    // href: "https://blog.csdn.net/gisarmory/article/details/112034622"
 }, {
     id: "GISer2021Flag",
     label: "2021给自己立个Flag",
